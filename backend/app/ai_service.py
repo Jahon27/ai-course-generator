4cb4db34e231cdd427794c57e97573187f5c338f
Jahon27 <jahonorotajieva@gmail.com>
2026-06-07 14:32:05 +0200

    prompt update

--- backend/app/ai_service.py
+++ backend/app/ai_service.py
@@ -1,3 +1,16 @@
+import os
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+
 def generate_quiz_from_text(lecture_text: str):
     lecture_text = lecture_text[:6000]
 
